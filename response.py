ba84b2ecc165b259d2fa0a14acc263091765ce24
sukanyaanil <137323328+sukanyaanil@users.noreply.github.com>
2023-06-29 11:21:43 +0530

    Add files via upload

--- response.py
+++ response.py
@@ -1,1 +1,35 @@
+import requests
+from requests import status_codes
+data=[]
+j=0
+x=requests.get('https://jsonplaceholder.typicode.com/posts')
+print(type(x.status_code))
+# print(x.url)
+# print(x.headers)
+# print(x.encoding)
+# print(x.elapsed)
+# print(x.close())
+# print(x.content)
+# print(x.cookies)
+# print(x.history)
+# print(x.is_redirect)
+# print(x.is_permanent_redirect)
+print(x.json())
+print(x.text)
+# print(x.request)
+# print(x.reason)
+print(x.links)
+if x.status_code == 200:
+    print('Success!')
+    for i in x.json():
+        data.append(i)
+elif x.status_code == 404:
+    print('Not Found.')
+else:
+    print("hi")
+print(type(data))
+for value in data:
+    print(value["id"])
 
+# print(x.status_code)
+# print(x.content)
